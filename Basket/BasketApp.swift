ac12151387779fabbcfacdc29d5a6d9da3c00dde
Thib1708 <thibault.giraudon@gmail.com>
2023-02-27 09:47:47 +0100

    Initial Commit

---
+++ Basket/BasketApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasketApp.swift
+//  Basket
+//
+//  Created by Thibault Giraudon on 27/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct BasketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
